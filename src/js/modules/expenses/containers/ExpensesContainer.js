4b8d07e5dae7b654bb7fcdad389c399a63d8b013
Anne McGinness <anne.mcginness@stormid.com>
2023-06-01 17:23:13 +0100

    micks fixes

--- src/js/modules/expenses/containers/ExpensesContainer.js
+++ src/js/modules/expenses/containers/ExpensesContainer.js
@@ -1,4 +1,5 @@
-import { h, useState, useEffect, useRef} from 'preact';
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import ExpensesNav from '../components/ExpensesNav';
 import Header from '../components/Header';
 import ExpensesList from '../components/ExpensesList';
@@ -6,31 +7,36 @@ import ExpensesList from '../components/ExpensesList';
 
 function ExpensesContainer () {
 
-    // const [expenses, setExpenses] = useState([]);
-    const items = [];
+    const [expenses, setExpenses] = useState([]);
+    const [newExpense, setnewExpense] = useState('');
+    // const items = [];
     // const [items, setItems] = useState([]);
+
+    const handleInput = event => setnewExpense(event.target.value);
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log("submitted!");
-        const name = event.currentTarget.item.value;
-        const item = {
-            name,
-            id: Date.now(),
-        };
-        // setItems(prevItems => [...prevItems, item]);
-        items.push(item);
-        console.log(`There are now ${items.length} in your state`);
-        event.target.reset();
-        displayItems()
+        setExpenses([...expenses, {name: newExpense, id: Date.now() }]);
+        setnewExpense('');
+        // console.log("submitted!");
+        // const name = event.currentTarget.item.value;
+        // const item = {
+        //     name,
+        //     id: Date.now(),
+        // };
+        // // setItems(prevItems => [...prevItems, item]);
+        // items.push(item);
+        // console.log(`There are now ${items.length} in your state`);
+        // event.target.reset();
+        // displayItems()
     }
 
-    function displayItems() {
-        console.log(items);
-        const html = items.map((item) => `<li>${item.name}</li>`).join('');
-        console.log(html);
-        // list.innerHTML = html;
-    }
+    // function displayItems() {
+    //     console.log(items);
+    //     const html = items.map((item) => `<li>${item.name}</li>`).join('');
+    //     console.log(html);
+    //     // list.innerHTML = html;
+    // }
 
 
     return (
@@ -39,11 +45,11 @@ function ExpensesContainer () {
         <div class="expenses-list">
         <h1 className="title">Recent transactions</h1>
         <form class="expenses" autocomplete="off" onSubmit={handleSubmit}>
-            <input type="text" name="item" id="item" />
+            <input type="text" name="item" id="item" value={newExpense} onInput={handleInput} />
             <button type="submit">+ Add Expense</button>
         </form>
         <ul className="list">
-            {items.map(item => (
+            {expenses.map(item => (
                 <li key={item.id}>{item.name}</li>
                 ))}
         </ul>
