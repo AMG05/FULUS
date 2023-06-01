36e88b2fab212e5448f88e7f79982a2ec1e57ddc
Anne McGinness <anne.mcginness@stormid.com>
2023-06-01 16:11:12 +0100

    a commit of errors

--- src/js/modules/expenses/components/Header.js
+++ src/js/modules/expenses/components/Header.js
@@ -1,29 +1,26 @@
 import { h } from 'preact';
+import { Router, Link } from 'preact-router';
 
 const Header = ({ }) => 
 <header class="header">
     <div class="wrap">
-        <div class="header__inner">
-            <a class="header__link" aria-label="Home" href="/">
-                <svg class="header__logo" src="src\img\logo.svg"></svg>
-            </a>
-            <nav aria-label="Main navigation" id="navigation" class="nav-primary js-toggle" data-toggle="js-toggle__nav">
-                <ul class="nav-primary__list">
-                    <li class="nav-primary_item">
-                        <a class="nav-primary__link" href="/Transactions/">Transactions</a>
-                    </li>
-                    <li class="nav-primary_item">
-                        <a class="nav-primary__link" href="/Expenses/">Add Expenses</a>
-                    </li>
-                    <li class="nav-primary_item">
-                        <a class="nav-primary__link" href="/Budget/">Budget</a>
-                    </li>
-                </ul>
-            </nav>
-            <a class="btn btn--header" href="/login/">Login</a>
+        <div class="outer-container">
+            <div class="navbar-container">
+                <div class="left-navbar">
+                    <img src="src\img\logo-png.png" width="127" />
+                    <div className="link-items">
+                    <a id="Transactions" href="/transactions">Transactions</a>
+                    <a id="Expenses" href="/expenses">Add expenses</a>
+                    <a id="Budget" href="/budget">Budget</a>
+                    </div>
+                </div>
+                <div class="right-navbar">
+                    <a className="btn btn-tertiary" href="/">Log in</a>
+                    <a className="btn" href="/">Sign up</a>
+                </div>
+            </div>
         </div>
     </div>
-
 </header>;
 
 export default Header;
--- src/js/modules/expenses/containers/ExpensesContainer.js
+++ src/js/modules/expenses/containers/ExpensesContainer.js
@@ -1,19 +1,56 @@
-import { h, useState} from 'preact';
+import { h, useState, useEffect, useRef} from 'preact';
 import ExpensesNav from '../components/ExpensesNav';
-import Header from '../components/header';
+import Header from '../components/Header';
 import ExpensesList from '../components/ExpensesList';
-import ExpensesForm from '../components/ExpensesForm';
+
 
 function ExpensesContainer () {
+
+    // const [expenses, setExpenses] = useState([]);
+    const items = [];
+    // const [items, setItems] = useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("submitted!");
+        const name = event.currentTarget.item.value;
+        const item = {
+            name,
+            id: Date.now(),
+        };
+        // setItems(prevItems => [...prevItems, item]);
+        items.push(item);
+        console.log(`There are now ${items.length} in your state`);
+        event.target.reset();
+        displayItems()
+    }
+
+    function displayItems() {
+        console.log(items);
+        const html = items.map((item) => `<li>${item.name}</li>`).join('');
+        console.log(html);
+        // list.innerHTML = html;
+    }
+
 
     return (
     <div>
         <Header />
-        <ExpensesNav />
-        <h1 className="title">I am the expenses container!</h1>
-        <p>this is a paragraph of annes app</p>
+        <div class="expenses-list">
+        <h1 className="title">Recent transactions</h1>
+        <form class="expenses" autocomplete="off" onSubmit={handleSubmit}>
+            <input type="text" name="item" id="item" />
+            <button type="submit">+ Add Expense</button>
+        </form>
+        <ul className="list">
+            {items.map(item => (
+                <li key={item.id}>{item.name}</li>
+                ))}
+        </ul>
+       
+        </div>
         <ExpensesList />
-        <ExpensesForm />
+        
         
     </div>
     );
